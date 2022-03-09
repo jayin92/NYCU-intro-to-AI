4a4dd9a7a650be54a478607fa8aa6d21dc8e3645
jayin92 <jayin920805@gmail.com>
2022-03-09 15:39:34 +0800

    add hw1

---
+++ HW1/Adaboost/dataset.py
@@ -0,0 +1,32 @@
+import os
+import cv2
+import numpy as np
+def loadImages(dataPath):
+    """
+    Load all Images in the folder and transfer a list of tuples. 
+    The first element is the numpy array of shape (m, n) representing the image.
+    (remember to resize and convert the parking space images to 36 x 16 grayscale images.) 
+    The second element is its classification (1 or 0)
+      Parameters:
+        dataPath: The folder path.
+      Returns:
+        dataset: The list of tuples.
+    """
+    # Begin your code (Part 1)
+    dataset = []
+    for item in os.listdir(os.path.join(dataPath, "car")):
+        img = cv2.imread(os.path.join(dataPath, "car", item), cv2.IMREAD_GRAYSCALE)
+        img = cv2.resize(img, (16, 36))
+        img = np.transpose(img)
+        data = (img, 1)
+        dataset.append(data)
+    
+    for item in os.listdir(os.path.join(dataPath, "non-car")):
+        img = cv2.imread(os.path.join(dataPath, "non-car", item), cv2.IMREAD_GRAYSCALE)
+        img = cv2.resize(img, (16, 36))
+        img = np.transpose(img)
+        data = (img, 0)
+        dataset.append(data)
+    # End your code (Part 1)
+    
+    return dataset
---
+++ HW1/Adaboost/detection.py
@@ -0,0 +1,60 @@
+import os
+from turtle import Turtle
+import cv2
+import matplotlib.pyplot as plt
+import numpy as np
+import utils
+from os import walk
+from os.path import join
+from datetime import datetime
+
+
+def crop(x1, y1, x2, y2, x3, y3, x4, y4, img) :
+    """
+    This function ouput the specified area (parking space image) of the input frame according to the input of four xy coordinates.
+    
+      Parameters:
+        (x1, y1, x2, y2, x3, y3, x4, y4, frame)
+        
+        (x1, y1) is the lower left corner of the specified area
+        (x2, y2) is the lower right corner of the specified area
+        (x3, y3) is the upper left corner of the specified area
+        (x4, y4) is the upper right corner of the specified area
+        frame is the frame you want to get it's parking space image
+        
+      Returns:
+        parking_space_image (image size = 360 x 160)
+      
+      Usage:
+        parking_space_image = crop(x1, y1, x2, y2, x3, y3, x4, y4, img)
+    """
+    left_front = (x1, y1)
+    right_front = (x2, y2)
+    left_bottom = (x3, y3)
+    right_bottom = (x4, y4)
+    src_pts = np.array([left_front, right_front, left_bottom, right_bottom]).astype(np.float32)
+    dst_pts = np.array([[0, 0], [0, 160], [360, 0], [360, 160]]).astype(np.float32)
+    projective_matrix = cv2.getPerspectiveTransform(src_pts, dst_pts)
+    croped = cv2.warpPerspective(img, projective_matrix, (360,160))
+    return croped
+
+
+def detect(dataPath, clf):
+    """
+    Please read detectData.txt to understand the format. 
+    Use cv2.VideoCapture() to load the video.gif.
+    Use crop() to crop each frame (frame size = 1280 x 800) of video to get parking space images. (image size = 360 x 160) 
+    Convert each parking space image into 36 x 16 and grayscale.
+    Use clf.classify() function to detect car, If the result is True, draw the green box on the image like the example provided on the spec. 
+    Then, you have to show the first frame with the bounding boxes in your report.
+    Save the predictions as .txt file (Adaboost_pred.txt), the format is the same as GroundTruth.txt. 
+    (in order to draw the plot in Yolov5_sample_code.ipynb)
+    
+      Parameters:
+        dataPath: the path of detectData.txt
+      Returns:
+        No returns.
+    """
+    # Begin your code (Part 4)
+    raise NotImplementedError("To be implemented")
+    # End your code (Part 4)
