16d3934e2311da4e96aa00cf728db13168ff43e7
jayin92 <jayin920805@gmail.com>
2022-03-09 21:37:18 +0800

    add hw1 tex file

--- HW1/Adaboost/dataset.py
+++ HW1/Adaboost/dataset.py
@@ -13,19 +13,22 @@ def loadImages(dataPath):
         dataset: The list of tuples.
     """
     # Begin your code (Part 1)
-    dataset = []
-    for item in os.listdir(os.path.join(dataPath, "car")):
-        img = cv2.imread(os.path.join(dataPath, "car", item))
-        img = cv2.resize(img, (36, 16))
-        img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-        data = (img, 1)
-        dataset.append(data)
+    dataset = [] # Declare an empty list to save the grayscale images
+
+    # Process images in "car" directory
+    for item in os.listdir(os.path.join(dataPath, "car")): # Use os.path.join to generate paths
+        img = cv2.imread(os.path.join(dataPath, "car", item)) # Read image from files
+        img = cv2.resize(img, (36, 16)) # Resize the image from (360, 160) to (36, 16)
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) # Convert image to grayscale image
+        data = (img, 1) # Create a tuple to store image and label and 
+        # because all images in "car" folder is the occupied parking space, the label is set to 1
+        dataset.append(data) # Append the tuple to the dataset list
     
-    for item in os.listdir(os.path.join(dataPath, "non-car")):
+    for item in os.listdir(os.path.join(dataPath, "non-car")): # Do the same thing as above but this time is for "non-car" folder
         img = cv2.imread(os.path.join(dataPath, "non-car", item))
         img = cv2.resize(img, (36, 16))
         img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-        data = (img, 0)
+        data = (img, 0) # Not occupied parking space, label is set to 0
         dataset.append(data)
     # End your code (Part 1)
     
